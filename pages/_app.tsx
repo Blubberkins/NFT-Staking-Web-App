0009df6c10b6fa7c7add7eb39adf39fe9363b503
Aaron Wang <z5308498@ad.unsw.edu.au>
2024-03-11 20:32:05 +1100

    removed unnecessary commented lines

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,9 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Sepolia } from "@thirdweb-dev/chains";
 
-// activeChain cannot be used for Sepolia as it isn't supported as one of the default chains, and is imported instead
-// const activeChain = "mumbai";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      //activeChain={activeChain}
       activeChain={Sepolia}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
